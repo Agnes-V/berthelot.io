6fdc1e029aac9e311736ff7cc358470a88905387
FBerthelot <florent.berthelot35@gmail.com>
2022-06-10 17:20:45 -0400

    :bug: fix typos

--- assets/subjects/courses/git-flow.js
+++ assets/subjects/courses/git-flow.js
@@ -4,11 +4,11 @@ module.exports = {
     Git, c'est génial, mais c'est aussi très complexe et même parfois fait passer les processus et les outils avant les individus.
     <br>
     <br>
-    C'est pourtant un outils que l'on peux tordre facilement pour qu'il s'adapte à nos besoin.
+    C'est pourtant un outil que l'on peut tordre facilement pour qu'il s'adapte à nos besoins.
 
     <br>
     <br>
-    Voyons ensemble les principaux flow Git qui existent et qui correspond au mieux à votre équipe.
+    Voyons ensemble les principaux flow Git qui existent et qui correspondent au mieux à votre équipe.
   `,
   url: 'https://slides.com/florentberthelot/git-flows',
 }
